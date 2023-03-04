1ebd537e1f6bf5607dccc0456c8c4cf4d0eebb5d
abrahamraies <abrahamraies@gmail.com>
2023-03-04 16:19:30 -0300

    Cambios en detalles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { AllcomponentsComponent } from './components/allcomponents/allcomponents.component';
 import { CertificadosComponent } from './components/certificados/certificados.component';
 
 const routes: Routes = [
   {path: 'certificados', component: CertificadosComponent},
+  {path: 'about', component: AboutComponent},
   { path: '**', redirectTo: '', pathMatch: 'full', component: AllcomponentsComponent}
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEnd';
+  title = 'Portfolio';
+  elem1 = false;
+  elem2 = false;
+  elem3 = false;
+  elem4 = false;
+  elem5 = false;
+  elem6 = false;
+
+  selected(text:string): void{
+    if(text == 'home'){
+      this.elem1 = true;
+      this.elem2 = false;
+      this.elem3 = false;
+      this.elem4 = false;
+      this.elem5 = false;
+      this.elem6 = false;
+    }else if (text == 'about'){
+      this.elem1 = false;
+      this.elem2 = true;
+      this.elem3 = false;
+      this.elem4 = false;
+      this.elem5 = false;
+      this.elem6 = false;
+    }else if (text == 'skills'){
+      this.elem1 = false;
+      this.elem2 = false;
+      this.elem3 = true;
+      this.elem4 = false;
+      this.elem5 = false;
+      this.elem6 = false;
+    }else if (text == 'resume'){
+      this.elem1 = false;
+      this.elem2 = false;
+      this.elem3 = false;
+      this.elem4 = true;
+      this.elem5 = false;
+      this.elem6 = false;
+    }else if (text == 'portfolio'){
+      this.elem1 = false;
+      this.elem2 = false;
+      this.elem3 = false;
+      this.elem4 = false;
+      this.elem5 = true;
+      this.elem6 = false;
+    }else if (text == 'contact'){
+      this.elem1 = false;
+      this.elem2 = false;
+      this.elem3 = false;
+      this.elem4 = false;
+      this.elem5 = false;
+      this.elem6 = true;
+    }
+  }
 }
