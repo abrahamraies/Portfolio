b555cbf694742dc87c8174e8722f8c7ab200c84b
abrahamraies <abrahamraies@gmail.com>
2024-08-23 21:59:16 +1000

    Changes in CV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxTypedJsModule } from 'ngx-typed-js';
-import { ProgressBarAngularModule } from "progress-bar-angular";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +29,6 @@ import { AllcomponentsComponent } from './components/allcomponents/allcomponents
   imports: [
     BrowserModule,
     NgxTypedJsModule,
-    ProgressBarAngularModule,
     ReactiveFormsModule,
     AppRoutingModule
   ],
