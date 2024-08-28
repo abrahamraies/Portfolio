4354c5c2fa4be6e468810f72f5c45553f2604283
abrahamraies <abrahamraies@gmail.com>
2024-08-29 08:45:28 +1000

    Update contact

--- src/app/components/proyects/proyect.component.ts
+++ src/app/components/proyects/proyect.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { LanguageService } from 'src/app/services/language.service';
 
+interface Project {
+  titleEs: string;
+  titleEn: string;
+  image: string;
+  altText: string;
+  filterClass: string;
+  demoLink: string;
+  repoLink: string;
+}
+
 @Component({
   selector: 'app-proyect',
   templateUrl: './proyect.component.html',
@@ -8,6 +18,46 @@ import { LanguageService } from 'src/app/services/language.service';
 })
 export class ProyectComponent implements OnInit {
   currentLanguage!: string;
+
+  projects: Project[] = [
+    {
+      titleEs: 'App de Noticias con IONIC',
+      titleEn: 'News App with IONIC',
+      image: 'assets/img/NoticiasPrincipal.png',
+      altText: 'News App with IONIC',
+      filterClass: 'filter-app',
+      demoLink: 'https://marvelapp.com/prototype/2i6hebeg',
+      repoLink: 'https://github.com/abrahamraies/App-de-noticias'
+    },
+    {
+      titleEs: 'App de Peliculas con IONIC',
+      titleEn: 'Movie App with IONIC',
+      image: 'assets/img/PeliculasPrincipal.png',
+      altText: 'Movie App with IONIC',
+      filterClass: 'filter-app',
+      demoLink: 'https://marvelapp.com/prototype/j8j4i3e',
+      repoLink: 'https://github.com/abrahamraies/peliculasApp'
+    },
+    {
+      titleEs: 'Web de Alquiler de vehiculos',
+      titleEn: 'Car rental (SpringBoot + Angular + MySql)',
+      image: 'assets/img/rentalsVenado.png',
+      altText: 'Car rental web site',
+      filterClass: 'filter-web',
+      demoLink: 'https://rentals-venado.web.app/',
+      repoLink: 'https://github.com/abrahamraies/Proyecto_alquiler_vehiculos'
+    },
+    {
+      titleEs: 'Web de Noticias con Angular',
+      titleEn: 'News Web with Angular',
+      image: 'assets/img/Imagen para portfolio.png',
+      altText: 'News Web with Angular',
+      filterClass: 'filter-web',
+      demoLink: 'https://drive.google.com/drive/folders/1Mqw7XWESNQhPS2zuWGrOewiWdKxyUUXn?usp=sharing',
+      repoLink: 'https://github.com/abrahamraies/DailyNews'
+    }
+  ];
+
   constructor(private languageService: LanguageService) { }
 
   ngOnInit(): void {
