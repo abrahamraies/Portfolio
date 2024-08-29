f79220523ed53df62d4dab708509387b919c8304
abrahamraies <abrahamraies@gmail.com>
2024-08-29 16:39:40 +1000

    Add new proyect

--- src/app/components/proyects/proyect.component.ts
+++ src/app/components/proyects/proyect.component.ts
@@ -23,7 +23,7 @@ export class ProyectComponent implements OnInit {
     {
       titleEs: 'App de Noticias con IONIC',
       titleEn: 'News App with IONIC',
-      image: 'assets/img/NoticiasPrincipal.png',
+      image: 'assets/img/proyects/NoticiasPrincipal.png',
       altText: 'News App with IONIC',
       filterClass: 'filter-app',
       demoLink: 'https://marvelapp.com/prototype/2i6hebeg',
@@ -32,16 +32,16 @@ export class ProyectComponent implements OnInit {
     {
       titleEs: 'App de Peliculas con IONIC',
       titleEn: 'Movie App with IONIC',
-      image: 'assets/img/PeliculasPrincipal.png',
+      image: 'assets/img/proyects/PeliculasPrincipal.png',
       altText: 'Movie App with IONIC',
       filterClass: 'filter-app',
       demoLink: 'https://marvelapp.com/prototype/j8j4i3e',
       repoLink: 'https://github.com/abrahamraies/peliculasApp'
     },
     {
-      titleEs: 'Web de Alquiler de vehiculos',
+      titleEs: 'Alquiler de vehiculos (SpringBoot + Angular + MySql)',
       titleEn: 'Car rental (SpringBoot + Angular + MySql)',
-      image: 'assets/img/rentalsVenado.png',
+      image: 'assets/img/proyects/rentalsVenado.png',
       altText: 'Car rental web site',
       filterClass: 'filter-web',
       demoLink: 'https://rentals-venado.web.app/',
@@ -50,11 +50,20 @@ export class ProyectComponent implements OnInit {
     {
       titleEs: 'Web de Noticias con Angular',
       titleEn: 'News Web with Angular',
-      image: 'assets/img/Imagen para portfolio.png',
+      image: 'assets/img/proyects/Imagen para portfolio.png',
       altText: 'News Web with Angular',
       filterClass: 'filter-web',
       demoLink: 'https://drive.google.com/drive/folders/1Mqw7XWESNQhPS2zuWGrOewiWdKxyUUXn?usp=sharing',
       repoLink: 'https://github.com/abrahamraies/DailyNews'
+    },
+    {
+      titleEs: 'Lista de tareas (.NET + Angular + MySql)',
+      titleEn: 'Task List (.NET + Angular + MySql)',
+      image: 'assets/img/proyects/TasksLists.png',
+      altText: 'Task List',
+      filterClass: 'filter-web',
+      demoLink: 'https://marvelapp.com/prototype/fd51hi6/screen/95440488',
+      repoLink: 'https://github.com/abrahamraies/task-list'
     }
   ];
 
